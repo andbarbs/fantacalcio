968276be39fbeb639335679987edab1a619a73b7
Andre <buia22@tiscali.it>
2025-03-30 15:19:10 +0200

    aggiunta la dependency jakarta.persistence-api
      > in questo modo è disponibile il tipo JPA CriteriaBuilder,
      al posto di HibernateCriteriaBuilder

--- src/main/java/hibernateDAL/HibernateGradeRepository.java
+++ src/main/java/hibernateDAL/HibernateGradeRepository.java
@@ -3,11 +3,10 @@ package hibernateDAL;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.query.criteria.HibernateCriteriaBuilder;
-
 import businessLogic.DAL.GradeRepository;
 import domainModel.Grade;
 import domainModel.Grade_;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Root;
@@ -17,20 +16,12 @@ public class HibernateGradeRepository extends HibernateEntityRepository implemen
 	public HibernateGradeRepository(SessionFactory sessionFactory) {
 		super(sessionFactory);
 	}
-
-//	@Override
-//	public List<Grade> getAllGrades() {
-//		return getSessionFactory().fromTransaction(
-//				session -> session.createSelectionQuery(
-//						"from Grade join fetch giocatore join fetch matchDay", Grade.class)
-//									.setReadOnly(true)
-//									.getResultList());
-//	}
 	
 	@Override
 	public List<Grade> getAllGrades() {
-		return getSessionFactory().fromTransaction(session -> {
-			HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+		return getSessionFactory().fromTransaction(session -> 
+		{
+			CriteriaBuilder builder = session.getCriteriaBuilder();
 			CriteriaQuery<Grade> query = builder.createQuery(Grade.class);
 			Root<Grade> gradeRoot = query.from(Grade.class);
 
