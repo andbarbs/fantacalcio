1c5b9956446a3763e0f370806d1e9183c601aac3
Andre <buia22@tiscali.it>
2025-03-30 14:39:21 +0200

    aggiunta la criteria query che fa i join fetch per getAllGrades()
    in questo modo,
    
       > il fetching della association può restare LAZY, ma
       > GradeRepository riesce a propagare istanze di Grade
         con fetching ricorsivo
         > questo permette al test su Grade.equals() di passare
    
    la criteria query è type-safe grazie all'uso delle classi del
    Metamodel, che quindi devono essere correttamente riconosciute
    dall'IDE

--- src/main/java/hibernateDAL/HibernateGradeRepository.java
+++ src/main/java/hibernateDAL/HibernateGradeRepository.java
@@ -3,9 +3,14 @@ package hibernateDAL;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 
 import businessLogic.DAL.GradeRepository;
 import domainModel.Grade;
+import domainModel.Grade_;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
 
 public class HibernateGradeRepository extends HibernateEntityRepository implements GradeRepository {
 
@@ -13,10 +18,29 @@ public class HibernateGradeRepository extends HibernateEntityRepository implemen
 		super(sessionFactory);
 	}
 
+//	@Override
+//	public List<Grade> getAllGrades() {
+//		return getSessionFactory().fromTransaction(
+//				session -> session.createSelectionQuery(
+//						"from Grade join fetch giocatore join fetch matchDay", Grade.class)
+//									.setReadOnly(true)
+//									.getResultList());
+//	}
+	
 	@Override
 	public List<Grade> getAllGrades() {
-		return getSessionFactory().fromTransaction(
-				session -> session.createSelectionQuery("from Grade", Grade.class).getResultList());
+		return getSessionFactory().fromTransaction(session -> {
+			HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<Grade> query = builder.createQuery(Grade.class);
+			Root<Grade> gradeRoot = query.from(Grade.class);
+
+			gradeRoot.fetch(Grade_.giocatore, JoinType.INNER);
+			gradeRoot.fetch(Grade_.matchDay, JoinType.INNER);
+
+			query.select(gradeRoot);
+			
+			return session.createSelectionQuery(query).setReadOnly(true).getResultList();
+		});
 	}
 
 }
