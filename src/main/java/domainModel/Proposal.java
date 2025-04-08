8f6ed52a4bf54643207a4252fcbb365fd2648cc8
nicco <niccozanni@gmail.com>
2025-04-08 17:57:50 +0200

    Proposal was annotated

--- src/main/java/domainModel/Proposal.java
+++ src/main/java/domainModel/Proposal.java
@@ -1,26 +1,57 @@
 package domainModel;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
 public class Proposal {
-    private FantaTeam ProposingTeam;
-    private FantaTeam ReceivingTeam;
-    private Player OfferedPlayer;
-    private Player RequestedPlayer;
+    public static enum Status {PENDING, ACCEPTED, REJECTED}
 
-    // Getters
-    public FantaTeam getProposingTeam() {
-        return ProposingTeam;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY )
+    private Contract offedredContract;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY )
+    private Contract requestedContract;
+
+    @Basic(optional = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Proposal() {}
+
+    public Proposal(Contract offedredContract, Contract requestedContract, Status status) {
+        this.offedredContract = offedredContract;
+        this.requestedContract = requestedContract;
+        this.status = status;
     }
 
-    public FantaTeam getReceivingTeam() {
-        return ReceivingTeam;
+    public Contract getOffedredContract() {
+        return offedredContract;
     }
 
-    public Player getGiocatoreProposto() {
-        return OfferedPlayer;
+    public Contract getRequestedContract() {
+        return requestedContract;
     }
 
-    public Player getGiocatoreRichiesto() {
-        return RequestedPlayer;
+    public Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Proposal proposal = (Proposal) o;
+        return Objects.equals(offedredContract, proposal.offedredContract) && Objects.equals(requestedContract, proposal.requestedContract) && status == proposal.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(offedredContract, requestedContract, status);
     }
 }
 
