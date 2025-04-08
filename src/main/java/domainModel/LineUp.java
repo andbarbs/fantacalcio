b96140c7b48f686c5775a610f9d2e104f0ec6600
nicco <niccozanni@gmail.com>
2025-04-08 17:42:30 +0200

    The class LineUp was created and annotated

--- src/main/java/domainModel/LineUp.java
+++ src/main/java/domainModel/LineUp.java
@@ -1,16 +1,48 @@
 package domainModel;
 
-public class LineUp {
-    private int mark;
-    private boolean starter;
+import jakarta.persistence.*;
 
-    // Getters
-    public int getMark() {
-        return mark;
+import java.util.Objects;
+
+@Entity
+public class LineUp {
+    public static enum Module{_343, _433, _352}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Basic(optional = false)
+    @Enumerated(EnumType.STRING)
+    private Module module;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    Match match;
+
+    public LineUp() {}
+
+    public LineUp(Module module, Match match) {
+        this.module = module;
+        this.match = match;
     }
 
-    public boolean isStarter() {
-        return starter;
+    public Module getModule() {
+        return module;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LineUp lineUp = (LineUp) o;
+        return module == lineUp.module && Objects.equals(match, lineUp.match);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(module, match);
     }
 }
-
