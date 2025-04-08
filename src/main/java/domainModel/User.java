83808569f64c4bf8d4862405c00edaf25638c788
nicco <niccozanni@gmail.com>
2025-04-08 16:25:52 +0200

    The class user was annotated

--- src/main/java/domainModel/User.java
+++ src/main/java/domainModel/User.java
@@ -1,4 +1,45 @@
 package domainModel;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Basic(optional=false)
+    public String email;
+
+    @Basic(optional=false)
+    public String password;
+
+    public User() {}
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
