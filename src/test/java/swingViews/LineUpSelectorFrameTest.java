16a0af5da5a89739d55199acc37a3e389e6d23c8
Andre <buia22@tiscali.it>
2025-05-10 21:31:12 +0200

    primo tevtativo di uso di AssertJ Swing e WindowBuilder

---
+++ src/test/java/swingViews/LineUpSelectorFrameTest.java
@@ -0,0 +1,37 @@
+package swingViews;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JLabelMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+public class LineUpSelectorFrameTest  {
+	
+	private FrameFixture window;
+	
+	private LineUpSelectorFrame view;
+	
+	
+
+//	@Override
+//	protected void onSetUp() {
+//		GuiActionRunner.execute(() -> {
+//			view = new LineUpSelectorFrame();
+//			return view;
+//		});
+//		window = new FrameFixture(robot(), view);
+//		window.show();		
+//	}
+	
+	@Test 
+	public void test() {
+		window.label(JLabelMatcher.withText("seleziona formazione"));
+	}
+
+
+}
---
+++ src/test/java/swingViews/stupidTestCase.java
@@ -0,0 +1,14 @@
+package swingViews;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class stupidTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
