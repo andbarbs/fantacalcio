4b8e1e10c3dd290390281238999a4c1b99bb9554
Andre <buia22@tiscali.it>
2025-06-29 21:37:41 +0200

    ripristinata interazione Combo <-> Button in CompetingPlayerSelector

--- src/main/java/swingViews/CompetingPlayerSelector.java
+++ src/main/java/swingViews/CompetingPlayerSelector.java
@@ -6,7 +6,7 @@ import domainModel.Player;
 
 public class CompetingPlayerSelector extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static Dimension SWING_PLAYER_SELECTOR_DIM = new Dimension(141, 230);
 	private CompetingComboBox<Player> comboBox;
 
@@ -18,42 +18,42 @@ public class CompetingPlayerSelector extends JPanel {
 		setPreferredSize(new Dimension(141, 230));
 		setMinimumSize(SWING_PLAYER_SELECTOR_DIM);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{1, 0};
-		gridBagLayout.rowHeights = new int[]{1, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 1, 0 };
+		gridBagLayout.rowHeights = new int[] { 1, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JLayeredPane layeredPane = new JLayeredPane();
 		GridBagConstraints gbc_layeredPane = new GridBagConstraints();
 		gbc_layeredPane.fill = GridBagConstraints.BOTH;
 		gbc_layeredPane.gridx = 0;
 		gbc_layeredPane.gridy = 0;
 		add(layeredPane, gbc_layeredPane);
-		
+
 		JLabel figureLabel = new JLabel("");
 		figureLabel.setIcon(new ImageIcon(getClass().getResource("/images/player_figure_120x225.png")));
 		figureLabel.setBounds(12, 0, 120, 225);
 		layeredPane.add(figureLabel);
-		
+
 		JLabel headLabel = new JLabel("");
 		layeredPane.setLayer(headLabel, 1);
 		headLabel.setBounds(12, 0, 120, 225);
 		layeredPane.add(headLabel);
 		headLabel.setIcon(new ImageIcon(getClass().getResource("/images/ronaldo_head_120x225.png")));
-		
+
 		JPanel panel = new JPanel();
 		layeredPane.setLayer(panel, 2);
 		panel.setOpaque(false);
 		panel.setBounds(0, 0, 143, 231);
 		layeredPane.add(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{0, 27, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 27, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		comboBox = new CompetingComboBox<Player>();
 		comboBox.setRenderer(new DefaultListCellRenderer() {
 			private static final long serialVersionUID = 1L;
@@ -81,15 +81,24 @@ public class CompetingPlayerSelector extends JPanel {
 		gbc_comboBox.gridx = 0;
 		gbc_comboBox.gridy = 0;
 		panel.add(comboBox, gbc_comboBox);
-		
+
 		JButton resetButton = new JButton("Reset");
 		resetButton.setEnabled(false);
 		GridBagConstraints gbc_resetButton = new GridBagConstraints();
 		gbc_resetButton.gridx = 0;
 		gbc_resetButton.gridy = 1;
 		panel.add(resetButton, gbc_resetButton);
+
+		// implements Combo Box -> Button interaction
+		comboBox.addActionListener(e -> resetButton.setEnabled(comboBox.getSelectedIndex() > -1));
+
+		// implements Button -> Combo Box interaction
+		resetButton.addActionListener(e -> {
+			getCompetingComboBox().setSelectedIndex(-1);
+			resetButton.setEnabled(false);
+		});
 	}
-	
+
 	public CompetingComboBox<Player> getCompetingComboBox() {
 		return comboBox;
 	}
