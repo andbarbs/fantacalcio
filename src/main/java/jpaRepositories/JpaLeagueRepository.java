d9d688acd984fda954af6aeb16de518124a04a33
niccolocappini <niccolocappini@gmail.com>
2025-07-31 10:17:25 +0200

    removed imports

--- src/main/java/jpaRepositories/JpaLeagueRepository.java
+++ src/main/java/jpaRepositories/JpaLeagueRepository.java
@@ -2,8 +2,6 @@ package jpaRepositories;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-
 import businessLogic.repositories.LeagueRepository;
 import domainModel.FantaUser;
 import domainModel.League;
