a9a484460df6bfb7b09aade36c3a6c61debafb69
nicco <niccozanni@gmail.com>
2025-09-03 17:11:57 +0200

    setVoteToPlayers was implemented

--- src/main/java/businessLogic/NewsPaperService.java
+++ src/main/java/businessLogic/NewsPaperService.java
@@ -1,13 +1,32 @@
 package businessLogic;
 
+import domainModel.Grade;
 import domainModel.Player;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 public class NewsPaperService {
 
-	public void setVoteToPlayer(Player player, int voto) {
-		System.out.println("Hello");
+	protected final TransactionManager transactionManager;
+
+	public NewsPaperService(TransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
 	}
 
+	public void setVoteToPlayers(Set<Grade> grades) {
+		transactionManager.inTransaction((context)->{
+			for (Grade grade : grades) {
+				if(grade.getMark() < -5 || grade.getMark() >25){
+					throw new IllegalArgumentException("Marks must be between -5 and 25");
+				}
+			}
+			for (Grade grade : grades) {
+				context.getGradeRepository().saveGrade(grade);
+			}
+		});
+	}
 	public void getPlayersToGrade() {
 	}
 
