bf280e077871521bd9bdaf1f681dd5dc0c73e999
nicco <niccozanni@gmail.com>
2025-10-11 16:26:38 +0200

    Aggiustato LineUpChooserTest

--- fantacalcio-app/src/test/java/gui/lineup/chooser/LineUpChooserTest.java
+++ fantacalcio-app/src/test/java/gui/lineup/chooser/LineUpChooserTest.java
@@ -798,7 +798,7 @@ public class LineUpChooserTest {
 			// GIVEN the SUT has been initialized with a Team and a Match
 			FantaTeam team = new FantaTeam("Dream Team", null, 30, null, new HashSet<>());			
 			Match match = new Match(
-					new MatchDaySerieA("Matchday 1", LocalDate.of(2025, 6, 19), 1),
+					new MatchDaySerieA("Matchday 1", 1, MatchDaySerieA.Status.FUTURE, team.getLeague()),
 					team, 
 					new FantaTeam("Challengers", null, 25, null, new HashSet<>()));
 			
